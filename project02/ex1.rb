b781aac5f758d629ce84616e2c029a127ad830dc
Kira Combs <kicombs@mines.edu>
2012-01-24 20:30:13 -0700

    Adding a line to the example

--- project02/ex1.rb
+++ project02/ex1.rb
@@ -5,5 +5,6 @@
 #puts 'Yay! Printing.'
 #puts "I'd much rather you 'not'."
 #puts 'I "said" do not touch this.'
-puts "This is a new line for the script."
+#puts "This is a new line for the script."
+puts 'This prints one line to the screen'
 
