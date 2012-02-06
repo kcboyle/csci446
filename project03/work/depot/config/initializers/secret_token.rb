30b6a9f6b80987c9479e36c6384c8255413b9190
Kira Combs <kicombs@mines.edu>
2012-02-05 18:30:19 -0700

    Added new work directory. Created default depot application using rails

---
+++ project03/work/depot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '1b3778edac99f528191ec209dcb28110df08104451cc84f5ed66b467ed29e0f9cd274f92649206d926897ac4eb435a470a0f35123c1c73e3b56a4de5f13fb714'
