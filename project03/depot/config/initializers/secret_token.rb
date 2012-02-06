555379d619d60b662aa8374a74030119a6c62240
Kira Combs <kicombs@mines.edu>
2012-02-05 18:22:27 -0700

    Remove duplicated directory

--- project03/depot/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Depot::Application.config.secret_token = 'e1b4b0ff8a0634e47e004181c0f756f21bd6dbd3f8dc62fbadb0652936cd6767d9366d6c76df00897e84df006f189200f9fad540adc3971c62c0cb85ea633f6e'
