c340dba2a94673e15845f610d88eafdb9d951f1c
Kira Combs <kicombs@mines.edu>
2012-02-07 12:31:39 -0700

    fixed error in show.html.erb. was missing a %>, and created a rake test error

--- project03/work/depot/test/functional/line_items_controller_test.rb
+++ project03/work/depot/test/functional/line_items_controller_test.rb
@@ -49,8 +49,7 @@ class LineItemsControllerTest < ActionController::TestCase
 
   test "should create line_item via ajax" do
     assert_difference('LineItem.count') do
-      xhr :post, :create, product_id:
-products(:ruby).id
+      xhr :post, :create, product_id: products(:ruby).id
     end
 
     assert_response :success
