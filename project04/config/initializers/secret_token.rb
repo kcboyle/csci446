3b3a4821708c918bf2d574313a10b0de6758be0c
Kira Combs <kicombs@mines.edu>
2012-02-15 21:48:07 -0700

    used rails new articles to create a boilerplate rails app

---
+++ project04/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Articles::Application.config.secret_token = '82615e617b4c39ea1b06863163ae75be956dae93245dff3c81df6268254e561d8bbeaccd3b60addbd7e916ac098409540e23fb0a81fe4b3077473fc380c767e0'
