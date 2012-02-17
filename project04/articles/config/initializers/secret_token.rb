a660fecb6fc5f2d3a07052836f3e03058ad47e0e
Kira Combs <kicombs@mines.edu>
2012-02-16 19:59:48 -0700

    recreated the application to fix the issue regarding the use of an extraneous page

---
+++ project04/articles/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Articles::Application.config.secret_token = '3e7b6f58f118265b4660fbfe59460bdc870b2f4027a83018920870805ff83e76b0099c1ec8e3ee5eb4374c66fddcf3f181fffc4ca9bfcc9c8afb4399ffe71a13'
