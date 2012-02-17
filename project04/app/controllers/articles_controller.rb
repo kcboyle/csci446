6500b3be284d84e98f9cbc75ce1fa8162bc6f67a
Kira Combs <kicombs@mines.edu>
2012-02-16 17:39:47 -0700

    changed the route for the web page so the article posting is visible on the home page. Still contemplating how to delete the current home page

--- project04/app/controllers/articles_controller.rb
+++ project04/app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
   def index
+    @posts = Post.order(:title)
   end
 end
--- project04/config/routes.rb
+++ project04/config/routes.rb
@@ -3,6 +3,8 @@ Articles::Application.routes.draw do
 
   get "articles/index"
 
+  resources :posts
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -52,8 +54,7 @@ Articles::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'articles#index'
-
+  root :to => 'posts#index', as: 'posts'
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
