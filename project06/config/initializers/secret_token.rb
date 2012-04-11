1b661b53d0bfd273e45faec4a3c6bb8207a8e042
Kira Combs <kira@debian.localdomain>
2012-04-10 20:55:49 -0600

    generated users and gamez scaffolds. trying to figure out authlogic

---
+++ project06/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gamez::Application.config.secret_token = '9427c82012bfb45e3c68d619d45bfbbf0b3b908b49977a7cb6bd8a8a08148621775980e7898b4457eaeabf4cdbb2bd98507444bc10ffbe233332a84380b64a0f'
---
+++ project06/db/migrate/20120410232409_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :crypted_password
+      t.string :password_salt
+      t.string :persistence_token
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :users
+  end
+end
