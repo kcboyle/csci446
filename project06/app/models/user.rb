9662fce339e152bb35959612dffc1f02d68a32f3
Kira Combs <kicombs@mines.edu>
2012-04-11 16:25:58 -0600

    Starting declarative authorization. edited the user model and created an authorization rules file

--- project06/app/models/user.rb
+++ project06/app/models/user.rb
@@ -5,4 +5,10 @@ class User < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name, :password, :username, :password_confirmation
   validates :first_name, :presence => true, :uniqueness => true
   validates :last_name, :presence => true, :uniqueness => true
+
+  def role_symbols
+    roles.map do |role|
+      role.name.downcase.to_sym
+    end
+  end
 end
