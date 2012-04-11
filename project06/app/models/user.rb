9569f22a60bbbdfd2247d0449785a1fc4ee3fd39
Kira Combs <kicombs@mines.edu>
2012-04-11 15:46:53 -0600

    updated user model to belong to role, and state that it has games

--- project06/app/models/user.rb
+++ project06/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  belongs_to :role  has_many :games, :dependent => :destroy
   acts_as_authentic
   attr_accessible :email, :first_name, :last_name, :password, :username, :password_confirmation
   validates :first_name, :presence => true, :uniqueness => true
