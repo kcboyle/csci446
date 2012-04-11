599a1ee384075011a9128496f39fc7037745e217
Kira Combs <kicombs@mines.edu>
2012-04-11 15:50:13 -0600

    edited the game model to belong to users. fixed small syntax error in user model

--- project06/app/models/user.rb
+++ project06/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  belongs_to :role  has_many :games, :dependent => :destroy
+  belongs_to :role  
+  has_many :games, :dependent => :destroy
   acts_as_authentic
   attr_accessible :email, :first_name, :last_name, :password, :username, :password_confirmation
   validates :first_name, :presence => true, :uniqueness => true
