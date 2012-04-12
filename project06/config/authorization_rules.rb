d9156cdee4f4f62eb5b2db11564bdcc8afb15e8d
Kira Combs <kicombs@mines.edu>
2012-04-11 18:56:12 -0600

    working on namespacing

--- project06/config/authorization_rules.rb
+++ project06/config/authorization_rules.rb
@@ -3,4 +3,8 @@ authorization do
     has_permission_on [:games, :users], :to =>[:index, :show, :new, :create, :edit, :update, :destroy]
 
   end
+
+  role :guest do
+    has_permission_on [:games], :to => [:index, :show]
+  end
 end
